b2f7f98e794ad0bd5cbbd0776dd5be08da6d7c86
Arbin Rai <hello>
2016-09-06 12:02:32 -0500

    Meters to Feet Working!

--- src/main/java/com/clara/DistanceConverter.java
+++ src/main/java/com/clara/DistanceConverter.java
@@ -23,8 +23,8 @@ public class DistanceConverter {
     public static int metersToFeet(int meters){
         //TODO! This method doesn't work yet. Can you fix it?
         //To convert meters to feet, multiply by 3
-
-        return 0;
+int ft=meters*3;
+        return ft;
     }
 
     // I know doubles would be more real world but we are keeping it simple
